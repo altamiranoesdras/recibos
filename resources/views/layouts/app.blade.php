e7ba077b67d353c47d6c0330fb5a358e3754e64e
Edras Altamirano <altamiranoesdras@gmail.com>
2022-05-16 20:40:01 -0600

    Eliminada marca

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,7 +53,6 @@
     <footer class="main-footer">
         <strong>
             Copyright &copy; 2014-{{anioActual()}}
-            <a href="https://solucionesaltamirano.com/">Soluciones Altamirano</a>.
         </strong>
         All rights reserved.
         <div class="float-right d-none d-sm-inline-block">
